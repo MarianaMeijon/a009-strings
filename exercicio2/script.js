874f2cda7a9c2fbb2c526ab08c78d674f7f4905c
MarianaMeijon <marianameijon1989@gmail.com>
2022-08-31 16:07:05 -0300

    Exercicios prontos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,14 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      "
+
+const minhaString1 = (minhaString.trim())
+console.log (minhaString1)
+
+const minhaString2 = (minhaString.length)
+const minhaString3 = (minhaString1.length)
+
+console.log (`A quantidade de caracteres antes da remoção dos espaços é : ${minhaString2}
+A quantidade de caracteres depois da remoçãi dos espaços é : ${minhaString3}`)  
+
+
+const minhaString4 = minhaString1.replace("________","sticioso")
+console.log (minhaString4)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,21 @@
-//Crie a const para a frase aqui
+// Crie a const para a frase aqui
+
+//a)
+const frase = (`Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair."`)
+console.log (frase)
+
+//b)
+const novafrasetrocando = frase.replace("verde","rosa").replace("azul","laranja")
+console.log (novafrasetrocando)
+
+//c)
+const fraseIncludesverde = novafrasetrocando.includes("verde") 
+const fraseIncludeslaranja = novafrasetrocando.includes("laranja")
+
+console.log (`A nova frase possui a palavra verde? ${fraseIncludesverde} 
+A nova frase inclui a palavra laranja? ${fraseIncludeslaranja}`)
+
+
+//**EXTRA**
+let fraseExtra = (`mas não deixe o gato sair`)
+console.log (fraseExtra.toUpperCase())
